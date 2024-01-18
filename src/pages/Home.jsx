da47c27dd882d58c494462e8276a43bee07d5fd7
Pierre R Galette <pierre.galette@protonmail.com>
2024-01-18 15:53:51 -0500

    Hid home page carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,59 +7,45 @@ import { Container, Card, Carousel, Row, Col } from "react-bootstrap";
 const Home = () => {
   // The home page showing the links to the 3 main pages of the app. You can leave this as it is.
   return (
-    
-    <Container fluid className="grid" style={{ paddingTop: "750px" }}>
-    
-  
+    <Container fluid className="grid" style={{ paddingTop: "80px" }}>
+      {/* <Carousel className="carousel-caption ml-100 mr-100">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://res.cloudinary.com/dxl0hu3v9/image/upload/v1703166330/01_wucvds.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h5>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Second slide&bg=eee"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h5>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h5>Third slide label</h5>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel> */}
 
-
-  <Carousel className="carousel-caption ml-100 mr-100">
-      <Carousel.Item> 
-        <img
-          className="d-block w-100"
-          src="https://res.cloudinary.com/dxl0hu3v9/image/upload/v1703166330/01_wucvds.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=eee"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel> 
-
-   
- 
-
-
-
-
-      
-     
-      
       <Link to="/cocktails">
         <Card
           className="d-flex justify-content-center"
@@ -80,10 +66,8 @@ const Home = () => {
           </Card.Body>
         </Card>
       </Link>
-      
-      
 
-<Link to="#">
+      <Link to="#">
         <Card
           className="d-flex justify-content-center"
           style={{
@@ -104,9 +88,8 @@ const Home = () => {
           </Card.Body>
         </Card>
       </Link>
- 
 
-<Link to="/new-cocktail">
+      <Link to="/new-cocktail">
         <Card
           className="d-flex justify-content-center"
           style={{
@@ -127,9 +110,6 @@ const Home = () => {
           </Card.Body>
         </Card>
       </Link>
-
-
-      
     </Container>
   );
 };
