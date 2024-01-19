03f1c250dc47db74acc03e195c92e3f7337ea85c
Pierre R Galette <pierre.galette@protonmail.com>
2024-01-18 21:21:34 -0500

    Added review provider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import * as bootstrap from "bootstrap";
 
 import { AuthProvider } from "./context/auth.context.jsx";
-import { CocktailProvider } from './context/cocktail.context.jsx'
+import { CocktailProvider } from "./context/cocktail.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <CocktailProvider>
-           <App />
+          <ReviewProvider>
+            <App />
+          </ReviewProvider>
         </CocktailProvider>
       </AuthProvider>
     </BrowserRouter>
