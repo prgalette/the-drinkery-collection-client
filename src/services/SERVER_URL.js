6e0d12936b52cf23b05ff1e2a2c639b8fb53844d
Pierre R Galette <pierre.galette@protonmail.com>
2024-01-19 13:02:11 -0500

    Swtiched back to Netlify

--- src/services/SERVER_URL.js
+++ src/services/SERVER_URL.js
@@ -1,2 +1,2 @@
-// export const SERVER_URL = 'https://the-drinkery-collection.adaptable.app/'
-export const SERVER_URL = 'http://localhost:4000'
+export const SERVER_URL = 'https://the-drinkery-collection.adaptable.app/'
+// export const SERVER_URL = 'http://localhost:4000'
